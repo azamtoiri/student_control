e10dc024331e9a606f12539b95103bcd543ccb34
spike <azamtoiri@gmail.com>
2024-03-05 20:10:11 +0300

    fear: registering page is done
    * Register page is now available

--- views/default_views/register_view.py
+++ views/default_views/register_view.py
@@ -1,16 +1,124 @@
+import asyncio
+import time
+
 import flet as ft
 from flet_route import Params, Basket
 
 from constants import LOGO_PATH
+from database.database import UserDatabase
+from user_controls.banners import SuccessBanner
 from user_controls.custom_input_field import CustomInputField
 from user_controls.input_filter import TextOnlyInputFilterRu
+from utils.exceptions import RequiredField, PasswordDontMatching, AlreadyRegistered, NotRegistered
+from utils.jwt_hash import hash_
+
+user_db = UserDatabase()
 
 
 def RegisterView(page: ft.Page, params: Params, basket: Basket) -> ft.View:
     # region: Functions
+    def display_register_form_error(field: str, message: str) -> None:
+        password_field2 = password2_field
+
+        fields = {
+            'first_name': first_name_field,
+            'last_name': last_name_field,
+            'middle_name': middle_name_field,
+            'group': group_field,
+            "course": course_field,
+            "age": age_field,
+            "email": email_field,
+            'username': username_field,
+            'password': password_field,
+            'password2': password_field2
+        }
+        if field in fields.keys():
+            # fields[field].input_box_content.error_text = message
+            asyncio.run(fields[field].set_fail(message))
+            page.update()
+
+    def display_success_banner(message: str) -> None:
+        banner = SuccessBanner(page, message)
+        page.show_banner(banner)
+        page.update()
+
+    def hide_banner() -> None:
+        if page.banner is not None:
+            page.banner.open = False
+            page.update()
+
     def login_click(e: ft.ControlEvent) -> None:
         e.page.route = '/login'
         e.page.update()
+
+    def register_click(e: ft.Container) -> None:
+        try:
+
+            # region: Form fields
+            first_name = str(first_name_field.input_box_content.value).strip() if len(
+                first_name_field.input_box_content.value) else None
+
+            last_name = str(last_name_field.input_box_content.value).strip() if len(
+                last_name_field.input_box_content.value) else None
+
+            middle_name = str(middle_name_field.input_box_content.value).strip() if len(
+                middle_name_field.input_box_content.value) else None
+
+            group = str(group_field.input_box_content.value).strip() if len(
+                group_field.input_box_content.value) else None
+
+            course = str(course_field.input_box_content.value).strip() if len(
+                course_field.input_box_content.value) else None
+
+            age = str(age_field.input_box_content.value).strip() if len(age_field.input_box_content.value) else None
+
+            email = str(email_field.input_box_content.value).strip() if len(
+                email_field.input_box_content.value) else None
+
+            username = str(username_field.input_box_content.value).strip() if len(
+                username_field.input_box_content.value) else None
+
+            password = str(password_field.input_box_content.value).strip() if len(
+                password_field.input_box_content.value) else None
+
+            password2 = str(password2_field.input_box_content.value).strip() if len(
+                password2_field.input_box_content.value) else None
+            # endregion
+
+            if password2 is None and username and password is None:
+                raise RequiredField('password')
+            elif password2 is None and username:
+                raise RequiredField('password2')
+            elif password != password2:
+                raise PasswordDontMatching('password2')
+            elif password and password2:
+                # hashing password
+                password = hash_(password2)
+
+            user_db.register_user(
+                first_name=first_name, last_name=last_name, middle_name=middle_name,
+                username=username, password=password, group=group, course=course, age=age, email=email
+            )
+
+            page.route = '/login'
+            page.update()
+            display_success_banner('Вы были успешно зарегистрированы')
+            time.sleep(2)
+            hide_banner()
+        except RequiredField as error:
+            display_register_form_error(error.field, str(error))
+
+        except NotRegistered as error:
+            display_register_form_error('username', str(error))
+
+        except AlreadyRegistered as error:
+            # display_warning_banner(str(error))
+            display_register_form_error('username', str(error))
+        except PasswordDontMatching as error:
+            display_register_form_error(error.field, str(error))
+        except Exception as error:
+            print(error)
+            # display_warning_banner(str(error))
 
     # endregion
 
@@ -68,6 +176,7 @@ def RegisterView(page: ft.Page, params: Params, basket: Basket) -> ft.View:
     register_button.width = 400
     register_button.height = 45
     register_button.icon = ft.icons.KEY
+    register_button.on_click = lambda e: register_click(e)
 
     login_button = ft.Container()
     login_button.alignment = ft.alignment.center
