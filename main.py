0bb27306976f6bba5113f7fec6f80b5ccbaec65f
spike <azamtoiri@gmail.com>
2024-03-05 19:33:54 +0300

    fear: update NotFoundedView
    * View centralized

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import flet as ft
+
+from user_controls.student_app_bar import STAppBar
 from utils.new_router import Routing
 from views.routes import all_routes
 
-from user_controls.student_app_bar import StudentAppBar, STAppBar
+
+def ViewNotFound(page: ft.Page, params, basket):
+    return ft.View(
+        "/notfound/404",
+        vertical_alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        controls=[
+            ft.Text(
+                "View / Page not found error 404",
+                size=30,
+            ),
+            ft.TextButton(
+                "Go Back OR ",
+                on_click=lambda e: page.go(page.views[-2].route),
+            )
+        ]
+    )
 
 
 def main(page: ft.Page):
@@ -11,11 +29,11 @@ def main(page: ft.Page):
     page.window_min_height = 900
     page.theme_mode = 'light'
 
-    route = Routing(page=page, app_routes=all_routes)
+    route = Routing(page=page, app_routes=all_routes, not_found_view=ViewNotFound)
     route.appbar = STAppBar()
     page.on_route_change = route.change_route
     page.appbar = route.appbar
-    page.go('/')
+    page.go('/akjlsdfh')
 
 
 ft.app(target=main)
