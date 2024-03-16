07592b3827629c111bbdc739634426c7c078bd2e
spike <azamtoiri@gmail.com>
2024-03-16 10:12:45 +0300

    fear: changing user avatar and can edit
    * Not connected to db

---
+++ user_controls/user_image_picker.py
@@ -0,0 +1,33 @@
+import flet as ft
+
+
+class UserImage(ft.UserControl):
+    def __init__(self, image_dir, on_click):
+        super().__init__()
+        self.image_dir = image_dir
+        self.on_click = on_click
+        self.ft_image = ft.Ref[ft.Image]()
+
+    def build(self):
+        self.display_view = ft.Stack(
+            controls=[
+                ft.Image(
+                    src=self.image_dir, width=180, height=180, border_radius=50, ref=self.ft_image,
+                    fit=ft.ImageFit.COVER
+                ),
+                ft.IconButton(
+                    ft.icons.EDIT,
+                    left=140, top=140,
+                    # left=120, right=0, bottom=120, top=0,
+                    on_click=self.on_click,
+                    icon_color=ft.colors.DEEP_ORANGE, highlight_color=ft.colors.ORANGE_ACCENT,
+                    bgcolor=ft.colors.ORANGE_50,
+                    opacity=0.75, icon_size=20,
+                    tooltip='Изменить изображение'
+                ),
+            ]
+        )
+        return self.display_view
+
+    def change_user_image(self):
+        self.update()
--- views/student_views/home_view.py
+++ views/student_views/home_view.py
@@ -1,51 +1,86 @@
+import os
+import shutil
+
 import flet as ft
 from flet_route import Params, Basket
 
+from user_controls.user_image_picker import UserImage
 from utils.routes_url import StudentRoutes
 
 
 def HomeView(page: ft.Page, params: Params, basket: Basket) -> ft.View:
+    # ref controls
+    upload_button = ft.Ref[ft.ElevatedButton]()
+    ft_image = ft.Ref[ft.Image]()
+
     # region: Functions
+    def on_dialog_result(e: ft.FilePickerResultEvent) -> None:
+        if e.files is None: return
+        upload_files(e)
+        page.update()
+
+    def upload_files(e: ft.FilePickerUploadFile):
+        uf = []
+        if page.web:
+            for f in pick_files.result.files:
+                # updating data in db
+                uf.append(ft.FilePickerUploadFile(f.name, upload_url=page.get_upload_url(f.name, 600)))
+            pick_files.upload(uf)
+        else:
+            for x in pick_files.result.files:
+                if os.path.exists(x.name):
+                    os.remove(x.name)
+                dest = os.path.join(os.getcwd(), "assets")
+                shutil.copy(x.path, f'{dest}')
+
+                # here will be updating data in db
+                _user_image_dir = f'/{x.name}'
+                user_avatar.ft_image.current.src = _user_image_dir
+                user_avatar.update()
+        page.update()
 
     # endregion
 
-    _user_avatar = ft.CircleAvatar(
-        radius=110,
-        foreground_image_url="assets/Fox_Hub_logo.png",
-    )
+    pick_files = ft.FilePicker(on_result=on_dialog_result)
+    page.overlay.append(pick_files)
+    page.update()
 
-    user_avatar = ft.Container(padding=ft.padding.all(10), bgcolor='grey', border_radius=150)
-    user_avatar.content = _user_avatar
+    user_image_dir = "default_user_image.png"
+    if user_image_dir != "":
+        user_avatar = UserImage(
+            f'/{user_image_dir}',
+            on_click=lambda _: pick_files.pick_files(),
+        )
+    else:
+        user_avatar = UserImage(
+            '/default_user_image.png',
+            on_click=lambda _: pick_files.pick_files(),
+        )
 
     username_text = ft.Text(size=40, color='black', value=f'{page.session.get("username")}')
 
     content = ft.Column([
-        ft.Row(alignment=ft.MainAxisAlignment.CENTER,
-               controls=[ft.Text('Домашняя страница', size=40, color=ft.colors.BLACK)]),
-        ft.Row([
-            ft.Container(content=user_avatar, padding=ft.padding.only(top=50, left=40, right=20)),
-            username_text,
-        ],
-            alignment=ft.MainAxisAlignment.START, expand=False
+        ft.Row(
+            alignment=ft.MainAxisAlignment.CENTER,
+            controls=[ft.Text('Домашняя страница', size=40, color=ft.colors.BLACK)]
         ),
-
-        ft.Row([
-            ft.Container(width=100, height=100, bgcolor='grey')
-        ],
-            expand=True
-        )
+        ft.Row(controls=[user_avatar, username_text]),
     ])
 
     # Background container for color and other
-    main_container = ft.Container(bgcolor='white', border_radius=8, padding=ft.padding.all(10))
+    main_container = ft.Container(
+        bgcolor='white', border_radius=8, padding=ft.padding.all(10)
+    )
     main_container.content = content
-    main_container.expand = True
     main_container.border_radius = 8
     main_container.shadow = ft.BoxShadow(
-        color='grey', offset=ft.Offset(1, 2), blur_radius=10,
+        color='grey',
+        offset=ft.Offset(1, 2),
+        blur_radius=10,
     )
 
     return ft.View(
+        scroll=ft.ScrollMode.AUTO,
         route=StudentRoutes.HOME_URL,
         controls=[main_container]
     )
