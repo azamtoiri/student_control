7fadeb92bbe1a76f1a97ad8b5af41df31f124d4a
spike <azamtoiri@gmail.com>
2024-03-23 14:03:58 +0300

    fear: add sql scripts for adding data to db

---
+++ sql scripts/users.sql
@@ -0,0 +1,7 @@
+insert into public.users (user_id, last_name, first_name, middle_name, age, "group", course, email, username, password, is_staff, is_superuser, created_at, user_image)
+values  (1, 'Николаева', 'Анна', 'Владимеровна', 22, '', 0, 'john.doe@gmail.com', 'john123', '$2b$12$vPWDJZG7l2DgjBMn.NOjhuG9MPNtxZTTu/OT/6YgVSSu4nclJptjm', true, null, '2024-03-21 22:20:58.468454 +00:00', null),
+        (2, 'Алексеев', 'Денис', 'Вадимович', 23, 'Р12', 3, 'jane.smith@gmial.com', 'jane123', '$2b$12$vPWDJZG7l2DgjBMn.NOjhuG9MPNtxZTTu/OT/6YgVSSu4nclJptjm', true, null, '2024-03-21 22:20:58.468454 +00:00', null),
+        (3, 'Маркочев', 'Данила', 'Альбертович', 19, 'ИСП12', 1, 'markoch.johnson@gmail.com', 'mark123', '$2b$12$vPWDJZG7l2DgjBMn.NOjhuG9MPNtxZTTu/OT/6YgVSSu4nclJptjm', false, null, '2024-03-21 22:20:58.468454 +00:00', null),
+        (4, 'Альберт', 'Юрий', 'Максимович', 24, 'ИСП32', 1, 'bob.johnson@gmail.com', 'albert123', '$2b$12$vPWDJZG7l2DgjBMn.NOjhuG9MPNtxZTTu/OT/6YgVSSu4nclJptjm', false, null, '2024-03-21 22:20:58.468454 +00:00', null),
+        (5, 'Валериева', 'Аннита', 'Бековна', 24, 'МП12', 1, 'bob.johnson@gmail.com', 'anita123', '$2b$12$vPWDJZG7l2DgjBMn.NOjhuG9MPNtxZTTu/OT/6YgVSSu4nclJptjm', false, null, '2024-03-21 22:20:58.468454 +00:00', null),
+        (6, 'Зоир', 'Фатоев', 'Ок', null, null, null, null, 'admin', '$2b$12$vbFTllH7nvKbRfkLU0u5wu.o0UV1FLSLwR9IBgcoEFo14y3JIhUXu', false, false, '2024-03-21 22:21:01.053280 +00:00', '99px_ru_wallpaper_283670_devushka_s_sirenevimi_volosami_i_dredami_v_profil.jpg');
